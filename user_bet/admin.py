de90ea81380912d454475e3f23140a02ce7a70f1
Petr <p.e.shirin@gmail.com>
2020-10-26 06:53:52 +0800

    add ajax_select for user bet

--- user_bet/admin.py
+++ user_bet/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import UserBet
 from django.urls import reverse
 from django.utils.html import format_html
+from ajax_select import make_ajax_form
+from sport_events.models import MatchEvent
 
 # Register your models here.
 
@@ -10,7 +12,9 @@ from django.utils.html import format_html
 class UserBetAdmin(admin.ModelAdmin):
     list_display = ('id', 'view_client', 'bet_type', 'user_bet', 'is_went', 'date_created', 'deleted')
     list_filter = ('user__client', 'bet_type', 'is_went', 'date_created', 'deleted')
-    filter_horizontal = ('events',)
+    form = make_ajax_form(UserBet, {
+        "events": "ajax_events"
+    })
 
     def view_client(self, obj: UserBet):
         url = reverse(f"admin:users_client_change", args=[obj.user.client.id])
